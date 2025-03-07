6eddcf0b38e921bc6c61f3a393f474584af5b332
FernandoChocKinal <fchoc-2020619@kinal.edu.gt>
2025-03-07 16:31:53 -0600

    Subida a CarritoCompras

---
+++ src/middlewares/user-validators.js
@@ -0,0 +1,75 @@
+import { body, param } from "express-validator";
+import { emailExists, usernameExists, userExists } from "../helpers/db-validators.js";
+import { validarCampos } from "./validate-fields.js";
+import { deleteFileOnError } from "./delete-file-on-error.js";
+import { handleErrors } from "./handle-errors.js";
+import { validateJWT } from "./validate-jwt.js";
+import { hasRoles } from "./validate-roles.js";
+
+export const registerValidator = [
+    body("name").notEmpty().withMessage("El nombre es requerido"),
+    body("username").notEmpty().withMessage("El username es requerido"),
+    body("email").notEmpty().withMessage("El email es requerido"),
+    body("email").isEmail().withMessage("No es un email válido"),
+    body("email").custom(emailExists),
+    body("username").custom(usernameExists),
+    body("password"),
+    validarCampos,
+    deleteFileOnError,
+    handleErrors
+];
+
+export const loginValidator = [
+    body("email").optional().isEmail().withMessage("No es un email válido"),
+    body("username").optional().isString().withMessage("Username es en formáto erróneo"),
+    body("password").isLength({ min: 2 }).withMessage("El password debe contener al menos 2 caracteres"),
+    validarCampos,
+    handleErrors
+];
+
+export const getUserByIdValidator = [
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(userExists),
+    validarCampos,
+    handleErrors
+];
+
+export const deleteUserValidator = [
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(userExists),
+    validarCampos,
+    handleErrors
+];
+
+export const updatePasswordValidator = [
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(userExists),
+    body("newPassword").isLength({ min: 4 }).withMessage("El password debe contener al menos 8 caracteres"),
+    validarCampos,
+    handleErrors
+];
+
+export const updateUserValidator = [
+    param("uid", "No es un ID válido").isMongoId(),
+    param("uid").custom(userExists),
+    validarCampos,
+    handleErrors
+];
+
+export const updateRoleValidator = [
+    validateJWT,
+    hasRoles("ADMIN_ROLE"),
+    param("uid").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("uid").custom(userExists),
+    body("newRole").isString().withMessage("El rol debe ser una cadena de texto").isIn(["ADMIN_ROLE", "CLIENT_ROLE"]).withMessage("Rol no válido"),
+    validarCampos,
+    handleErrors
+];
+
+
+export const confirmacionEliminarCuentaValidator = [
+    validateJWT,
+    body("password").notEmpty().withMessage("La contraseña es requerida"),
+    validarCampos,
+    handleErrors
+];
---
+++ src/user/user.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { getUserById, getUsers, deleteUser, updatePassword,updateUser, updateRole, eliminarCuenta } from "./user.controller.js";
+import { getUserByIdValidator, deleteUserValidator, updatePasswordValidator, updateUserValidator, updateRoleValidator, confirmacionEliminarCuentaValidator} from "../middlewares/user-validators.js";
+
+const router = Router();
+
+router.get("/findUser/:uid", getUserByIdValidator, getUserById);
+
+router.get("/", getUsers);
+
+router.delete("/deleteUser/:uid", deleteUserValidator, deleteUser);
+
+router.patch("/updatePassword/:uid", updatePasswordValidator, updatePassword);
+
+router.put("/updateUser/:uid", updateUserValidator, updateUser);
+
+router.patch("/updateRole/:uid", updateRoleValidator, updateRole);
+
+router.delete("/eliminarCuenta/:uid", confirmacionEliminarCuentaValidator, eliminarCuenta);
+
+export default router;
